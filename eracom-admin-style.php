2ff9947e8bc9d8577d0f4bd54934a895bd883387
Manuel Schmalstieg <git.code@ms-studio.net>
2026-06-17 12:52:01 +0200

    Styles de titre pour éditeur standard
    Pour une certaine raison, l'aspect typographique était perturbé dans l'éditeur de blocs standard.
    
    Ces styles rétablissent une bonne distinction des titres.

--- eracom-admin-style.php
+++ eracom-admin-style.php
@@ -4,6 +4,21 @@
 custom css for admin
 */
 
+add_action('enqueue_block_editor_assets', function() {
+    wp_add_inline_style('wp-edit-blocks', '
+        :root {
+            font-size: 16px !important;
+        }
+        .editor-styles-wrapper h1 { font-size: 2em !important; }
+        .editor-styles-wrapper h2 { font-size: 1.5em !important; }
+        .editor-styles-wrapper h3 { font-size: 1.17em !important; }
+        .editor-styles-wrapper h4 { font-size: 1em !important; }
+        .editor-styles-wrapper h5 { font-size: 0.83em !important; }
+        .editor-styles-wrapper h6 { font-size: 0.67em !important; }
+    ');
+});
+
+
  function eracom_admin_css() {
     echo '<style type="text/css">
     
