30f123464afaec6e0a0f9e324fd81ea23d165f10
Manuel Schmalstieg <git.code@ms-studio.net>
2026-06-17 13:36:16 +0200

    Désactive le post-type Formations
    Elles deviennent de simples pages, pas de Post-Type spécifique

--- eracom-post-types.php
+++ eracom-post-types.php
@@ -67,45 +67,5 @@ function eracom_register_post_types() {
 			 		) 
 			 );
 		
-		
-		register_post_type(
-				'eracom_formation', array(	
-					'label' => 'Formation',
-					'description' => 'Formations ERACOM',
-					'public' => true,
-					'show_ui' => true,
-					'show_in_menu' => true,
-					 'menu_icon' => 'dashicons-welcome-learn-more', // src: https://developer.wordpress.org/resource/dashicons/
-					'capability_type' => 'post',
-					'hierarchical' => false,
-					'has_archive'		 => false,
-					'rewrite' => array('slug' => 'formation'),
-					'query_var' => true,
-					'exclude_from_search' => true,
-					'menu_position' => 6,
-					'supports' => array(
-						'title',
-						// 'editor',
-						// 'revisions',
-						// 'thumbnail',
-						),
-					'labels' => array (
-				  	  'name' => 'Formations',
-				  	  'singular_name' => 'Formation',
-				  	  'menu_name' => 'Formations',
-				  	  'add_new' => 'Ajouter',
-				  	  'add_new_item' => 'Ajouter',
-				  	  'edit' => 'Modifier',
-				  	  'edit_item' => 'Modifier',
-				  	  'new_item' => 'Nouvelle formation',
-				  	  'view' => 'Afficher',
-				  	  'view_item' => 'Afficher',
-				  	  'search_items' => 'Rechercher',
-				  	  'not_found' => 'Aucun résultat',
-				  	  'not_found_in_trash' => 'Aucun résultat',
-				  	  'parent' => 'Élément Parent',
-				),
-			) 
-		);
 
 } // end eracom_register_post_types
