62b89190296eca66df92857198bba208d2c2b6b2
Manuel Schmalstieg <git.code@ms-studio.net>
2026-06-17 12:53:03 +0200

    désactive project-infos dans le plugin
    On préfère gérer les champs Project Infos dans l'interface WordPress, donc on désactive cela dans le plugin.

--- eracom-acf.php
+++ eracom-acf.php
@@ -12,7 +12,7 @@ if ( function_exists('acf_add_local_field_group') ) {
 
 	include_once (plugin_dir_path(__FILE__).'acf/formation-infos.php');
 
-	include_once (plugin_dir_path(__FILE__).'acf/projet-infos.php');
+	// include_once (plugin_dir_path(__FILE__).'acf/projet-infos.php');
 
 }
 
